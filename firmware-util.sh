440b6b1442a91d460456a1236c82ccd4d6a47e52
ModdedPixels <goodaccotnew@gmail.com>
2025-11-01 19:57:33 -0400

    Update firmware-util.sh

--- firmware-util.sh
+++ firmware-util.sh
@@ -17,7 +17,7 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 #where the stuff is
-script_url="https://raw.githubusercontent.com/ModdedPixels/Chromebook-UEFI/refs/heads/main/"
+script_url="https://raw.githubusercontent.com/ModdedPixels/scripts/main/firmware-util.sh"
 
 #ensure output of system tools in en-us for parsing
 export LC_ALL=C
